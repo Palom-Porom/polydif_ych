317db5ba4efb543a922cc9c894077cf9b356fe24
Егор Деркунский <114829512+Palom-Porom@users.noreply.github.com>
2024-06-04 10:47:51 +0300

    Rough version

--- lib/polydif_ych.rb
+++ lib/polydif_ych.rb
@@ -3,6 +3,14 @@
 require_relative "polydif_ych/version"
 
 module PolydifYch
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.polydif(polynomial, symb)
+    # if only multiplier
+    polynomial.(/([+\-])?(\d+)/, "")
+    # if other variable
+    polynomial.gsub(/([+\-])?(\d+).(\^\d+)?/, "")
+    # if multiplier = 1
+    polynomial.gsub(/([+\-])?#{symb}\^(\d+)/, /#{$1.to_i == 1 ? $0 + 1 : $0 + ($1.to_i).to_s + "*" + symb + ($1 / 2 == 1 ? "" : "^" + $1/2))}/)
+    # if multiplier != 1
+    polynomial.gsub(/([+\-])?(\d+)\*#{symb}\^(\d+)/, /#{$2.to_i == 1 ? $0 + $1 : $0 + ($1.to_i * $2.to_i).to_s + "*" + symb + ($2 / 2 == 1 ? "" : "^" + $2/2))}/)
+  end
 end
--- test/test_polydif_ych.rb
+++ test/test_polydif_ych.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "polydif_ych"
 
 class TestPolydifYch < Minitest::Test
   def test_that_it_has_a_version_number
@@ -9,5 +10,13 @@ class TestPolydifYch < Minitest::Test
 
   def test_it_does_something_useful
     assert true
+  end
+
+  def test_example
+    assert_equal("2*x+4", PolydifYch.polydif("x^2+4*x+3+y", "x"), "Example test didn't pass")
+  end
+
+  def test_simplest
+    assert_equal("2*x", PolydifYch.polydif("x^2", "x"), "Example test didn't pass")
   end
 end
