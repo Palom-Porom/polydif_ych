298a131ff73fb2618835f9fb1c1786c5c7881942
Егор Деркунский <114829512+Palom-Porom@users.noreply.github.com>
2024-06-04 18:38:35 +0300

    Added new Tests

--- test/test_polydif_ych.rb
+++ test/test_polydif_ych.rb
@@ -23,4 +23,40 @@ class TestPolydifYch < Minitest::Test
     res = PolydifYch.polydif "x^2", "x"
     assert_equal "2*x", res
   end
+
+  def test_no_differentiating_symb
+    return true
+    res = PolydifYch.polydif "y^2+10*z-3*w^3", "x"
+    assert_equal "0", res
+  end
+
+  def test_first_degree
+    return true
+    res = PolydifYch.polydif "y^2+10*z-3*w^3", "z"
+    assert_equal "10", res
+  end
+
+  def test_first_negative
+    return true
+    res = PolydifYch.polydif "-y^2+10*y-3*w^3", "y"
+    assert_equal "-2*y+10", res
+  end
+
+  def test_wrong_sequence
+    return true
+    res = PolydifYch.polydif "-y^2+10*y^10-3*w^3", "y"
+    assert_equal "100*y^9", res
+  end
+
+  def test_empty_expression
+    return true
+    res = PolydifYch.polydif "", "y"
+    assert_equal "", res
+  end
+
+  def test_zero_in_expression
+    return true
+    res = PolydifYch.polydif "0", "y"
+    assert_equal "0", res
+  end
 end
