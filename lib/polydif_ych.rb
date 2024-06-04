cdbd1b50dbff8d127d1fc00ca8665962e1b49ac5
Егор Деркунский <114829512+Palom-Porom@users.noreply.github.com>
2024-06-04 18:17:36 +0300

    Additional Small Fixes

--- lib/polydif_ych.rb
+++ lib/polydif_ych.rb
@@ -10,23 +10,23 @@ require_relative "polydif_ych/version"
 module PolydifYch
   def polydif(polynomial, symb)
     # if only multiplier
-    polynomial.gsub(/([+-])?(\d+)/, "")
+    polynomial.gsub /([+-])?(\d+)/, ""
     # if other variable
-    polynomial.gsub(/([+-])?(\d+).(\^\d+)?/, "")
+    polynomial.gsub /([+-])?(\d+).(\^\d+)?/, ""
     # if multiplier = 1
-    polynomial.gsub(/([+-])#{symb}\^(\d+)/, /#{
+    polynomial.gsub /([+-])#{symb}\^(\d+)/, /#{
       # puts $2.to_s
       if $2.to_s.to_i == 1
         $1.to_s + "1"
       else
         res = $1.to_s + $2.to_s + "*" + symb
         if $2.to_s.to_i / 2 != 1
-          res += (($2.to_s.to_f) / 2).to_s
+          res += ($2.to_s.to_f / 2).to_s
         end
         res
-      end}/)
+      end}/
     # if multiplier = 1
-    polynomial.gsub(/#{symb}\^(\d+)/, /#{
+    polynomial.gsub /#{symb}\^(\d+)/, /#{
       if $1.to_i == 1
         "1"
       else
@@ -35,20 +35,25 @@ module PolydifYch
           res += $1.to_s / 2
         end
         res
-      end}/)
+      end}/
     # if multiplier != 1
-    polynomial.gsub(/([+\-])(\d+)\*#{symb}\^(\d+)/, /#{
+    polynomial.gsub /([+\-])(\d+)\*#{symb}\^(\d+)/, /#{
       if $3.to_i == 1
         $1.to_s + $2.to_s
       else
         $1.to_s + ($2.to_i * $3.to_i).to_s + "*" + symb + ($3.to_i - 1 == 1 ? "" : "^" + $3 / 2)
-      end}/)
-    polynomial.gsub(/(\d+)\*#{symb}\^(\d+)/, /#{
+      end}/
+    polynomial.gsub /(\d+)\*#{symb}\^(\d+)/, /#{
       if $2.to_i == 1
         $1.to_s
       else
         ($1.to_i * $2.to_i).to_s + "*" + symb + ($2.to_i - 1 == 1 ? "" : "^" + $2 / 2)
-      end}/)
+      end}/
     polynomial
   end
 end
+
+# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/PerceivedComplexity
