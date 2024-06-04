f1b86475489c1550922d300bd3b7389d96c0d9ab
Егор Деркунский <114829512+Palom-Porom@users.noreply.github.com>
2024-06-04 12:28:00 +0300

    Fixed one problem and Test Another
    Forgot about . in one place
    But now there are some problems with nil numbers

--- lib/polydif_ych.rb
+++ lib/polydif_ych.rb
@@ -3,28 +3,47 @@
 require_relative "polydif_ych/version"
 
 module PolydifYch
-  def self.polydif(polynomial, symb)
+  def polydif(polynomial, symb)
     # if only multiplier
-    polynomial.(/([+\-])?(\d+)/, "")
+    polynomial.gsub(/([+\-])?(\d+)/, "")
     # if other variable
     polynomial.gsub(/([+\-])?(\d+).(\^\d+)?/, "")
     # if multiplier = 1
-    polynomial.gsub(/([+\-])?#{symb}\^(\d+)/, /#{
+    polynomial.gsub(/([+\-])#{symb}\^(\d+)/, /#{
       if $2.to_i == 1
-        $1 + 1
+        $1 + "1"
       else
+        puts($1.class)
         res = $1 + ($2.to_i).to_s + "*" + symb
         if $2 / 2 != 1
           res = res + $2 / 2
         end
         res
       end}/)
+    # if multiplier = 1
+    polynomial.gsub(/#{symb}\^(\d+)/, /#{
+      if $1.to_i == 1
+        "1"
+      else
+        res = ($1.to_i).to_s + "*" + symb
+        if $1 / 2 != 1
+          res = res + $1 / 2
+        end
+        res
+      end}/)
     # if multiplier != 1
-    polynomial.gsub(/([+\-])?(\d+)\*#{symb}\^(\d+)/, /#{
+    polynomial.gsub(/([+\-])(\d+)\*#{symb}\^(\d+)/, /#{
       if $3.to_i == 1
         $1 + $2
       else
         $1 + ($2.to_i * $3.to_i).to_s + "*" + symb + ($3.to_i - 1 == 1 ? "" : "^" + $3 / 2)
       end}/)
+    polynomial.gsub(/(\d+)\*#{symb}\^(\d+)/, /#{
+      if $2.to_i == 1
+        $1
+      else
+        ($1.to_i * $2.to_i).to_s + "*" + symb + ($2.to_i - 1 == 1 ? "" : "^" + $2 / 2)
+      end}/)
+    polynomial
   end
 end
